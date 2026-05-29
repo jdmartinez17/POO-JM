9a9366cfeb0fea94ecf6b81879a4054c8e5963c1
Juan David Marinez Vega <martinezvegajuandavid3@gmail.com>
2026-05-28 20:28:25 -0500

    feat (ejercio 2):implementar CuentaBancaria con atributos privados y demostración de acceso

---
+++ src/taller7/Cuentabancaria.java
@@ -0,0 +1,37 @@
+package taller7;
+
+class CuentaBancaria {
+    private String numeroCuenta;
+    private double saldo;
+    public String tipoCuenta;
+
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta){
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+    public double getSaldo(){
+        return this.saldo;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    public void mostrarDetalles() {
+        System.out.println("Detalles de la cuenta");
+        System.out.println("Número de Cuenta = " + this.numeroCuenta);
+        System.out.println("Tipo de Cuenta = " + this.tipoCuenta);
+        System.out.println("Saldo disponible = $" + this.saldo);
+    }
+
+}
+class main {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria("987654321",250000, "Ahorros");
+        cuenta.mostrarDetalles();
+
+        // Hay que quitar las barras de la linea de abajo para ver el error de compilacion
+        //System.out.println(cuenta.numeroCuenta);
+
+    }
+
+}
